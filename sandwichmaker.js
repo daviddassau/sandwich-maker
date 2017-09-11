81fe1cfb9f921c4d6d93286e701ddab5cb1ec95c
David Dassau <todieahero@hotmail.com>
2017-09-11 12:20:36 -0500

    got all the other toppings to print to console

--- sandwichmaker.js
+++ sandwichmaker.js
@@ -3,14 +3,10 @@
 // Variable to hold the final price. Default to 0.
 var finalSandwichPrice = 0;
 
-// Variable to hold the bread that the user selects
-var selectedBread;
 
-let bread = document.getElementById("breadDiv");
-// console.log(bread);
-let myBread = SandwichMaker.getBread();
-// console.log("Bread Option", (myBread));
-
+var selectedBread; // Variable to hold the bread that the user selects
+let bread = document.getElementById("breadDiv"); // Getting the bread div from html
+let myBread = SandwichMaker.getBread(); // Get the getBread getter from bread.js
 
 bread.addEventListener("change", (e) => {
 	selectedBread = e.target.value;
@@ -24,3 +20,85 @@ bread.addEventListener("change", (e) => {
 		}
 	}
 });
+
+
+var selectedMeat; // Variable to hold the meat that the user selects
+let meat = document.getElementById("meatDiv"); // Getting the meat div from html
+let myMeat = SandwichMaker.getMeat(); // Get the getMeat getter from meat.js
+
+meat.addEventListener("change", (e) => {
+	selectedMeat = e.target.value;
+	for(let key in myMeat){
+		if(selectedMeat === key && e.target.checked){
+			finalSandwichPrice += myMeat[key];
+			console.log(finalSandwichPrice);
+		} else if(selectedMeat === key && e.target.checked === false){
+			finalSandwichPrice -= myMeat[key];
+			console.log(finalSandwichPrice);
+		}
+	}
+});
+
+
+var selectedCheese; // Variable to hold the cheese that the user selects
+let cheese = document.getElementById("cheeseDiv"); // Getting the cheese div from html
+let myCheese = SandwichMaker.getCheese(); // Get the getCheese getter from cheese.js
+
+cheese.addEventListener("change", (e) => {
+	selectedCheese = e.target.value;
+	for(let key in myCheese){
+		if(selectedCheese === key && e.target.checked){
+			finalSandwichPrice += myCheese[key];
+			console.log(finalSandwichPrice);
+		} else if(selectedCheese === key && e.target.checked === false){
+			finalSandwichPrice -= myCheese[key];
+			console.log(finalSandwichPrice);
+		}
+	}
+});
+
+
+var selectedCondiments; // Variable to hold the condiments that the user selects
+let condiments = document.getElementById("condimentsDiv"); // Getting the condiments div from html
+let myCondiments = SandwichMaker.getCondiments(); // Get the getCondiments getter from condiments.js
+
+condiments.addEventListener("change", (e) => {
+	selectedCondiments = e.target.value;
+	for(let key in myCondiments){
+		if(selectedCondiments === key && e.target.checked){
+			finalSandwichPrice += myCondiments[key];
+			console.log(finalSandwichPrice);
+		} else if(selectedCondiments === key && e.target.checked === false){
+			finalSandwichPrice -= myCondiments[key];
+			console.log(finalSandwichPrice);
+		}
+	}
+});
+
+
+var selectedVeggies; // Variable to hold the veggies that the user selects
+let veggies = document.getElementById("veggiesDiv"); // Getting the veggies div from html
+let myVeggies = SandwichMaker.getVeggies(); // Get the getCondiments getter from veggies.js
+
+veggies.addEventListener("change", (e) => {
+	selectedVeggies = e.target.value;
+	for(let key in myVeggies){
+		if(selectedVeggies === key && e.target.checked){
+			finalSandwichPrice += myVeggies[key];
+			console.log(finalSandwichPrice);
+		} else if(selectedVeggies === key && e.target.checked === false){
+			finalSandwichPrice -= myVeggies[key];
+			console.log(finalSandwichPrice);
+		}
+	}
+});
+
+
+
+
+
+
+
+
+
+
